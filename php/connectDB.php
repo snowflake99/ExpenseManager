b3a8ef8b123796ffa53c934bd273dd79bfac9269
sanjoy saha <sanjoysaha85@gmail.com>
2015-12-10 21:18:51 -0500

    formatting php file

--- php/connectDB.php
+++ php/connectDB.php
@@ -1,10 +1,11 @@
 <?php
-$username = "sanjoy";
-$password = "sanjoy";
-$hostname = "localhost"; 
+	$username = "sanjoy";
+	$password = "sanjoy";
+	$hostname = "localhost"; 
+	
+	//connection to the database
+	$dbhandle = mysql_connect($hostname, $username, $password) 
+	  or die("Unable to connect to MySQL");
 
-//connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password) 
-  or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
+	echo "Connected to MySQL";
 ?>
