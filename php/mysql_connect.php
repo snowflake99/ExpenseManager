809e03d70938c2187dbc02cdfbfdf16e0709b51a
sanjoy saha <sanjoysaha85@gmail.com>
2015-12-10 23:54:36 -0500

    Added save and exit in vert nav

--- php/mysql_connect.php
+++ php/mysql_connect.php
@@ -7,5 +7,14 @@
 	$dbhandle = mysql_connect($hostname, $username, $password) 
 	  or die("Unable to connect to MySQL");
 
-	echo "Connected to MySQL";
+	$selected = mysql_select_db("testdb",$dbhandle) 
+	  or die("Could not select examples");
+
+	$result = mysql_query("SELECT Date, Category, Description, Amount FROM _01_2015_");
+
+	while ($row = mysql_fetch_array($result)) {
+	   echo "+Date:".$row{'Date'}."Category:".$row{'Category'}."Description".$row{'Description'}."Amount".$row{'Amount'};
+
+	}	
+	//echo $result;
 ?>
