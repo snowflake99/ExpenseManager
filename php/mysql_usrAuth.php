8ba7a7a65b37a72ececf174743c79295e260a881
sanjoy saha <sanjoysaha85@gmail.com>
2015-12-12 22:51:42 -0500

    redirecting to login page if the user is not loggedin

--- php/mysql_usrAuth.php
+++ php/mysql_usrAuth.php
@@ -14,14 +14,22 @@
     $result = mysql_query("SELECT username, password FROM usrAuth");
 
     $msg="Login failure";
+    $success=false;
     while ($row = mysql_fetch_array($result)) {
        if (strcmp ($row{'username'}, $username) == 0 && 
                strcmp ($row{'password'}, $password) == 0) {
 	$msg="Login successful";
         header("Location: ../home"); 
+        $success=true;
 	break;
        }
     }
+    
+    if ($success == true)	{
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+    } 
 
     echo $msg;
 
