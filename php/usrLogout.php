68d40200a264ccd1ce4f413d8f4725c0dee9edc0
sanjoy saha <sanjoysaha85@gmail.com>
2015-12-16 10:55:04 -0500

    fixed logout issue

--- php/usrLogout.php
+++ php/usrLogout.php
@@ -1,13 +1,15 @@
 <?php
-    if (!isset($_SESSION['loggedin']))	{
-	if ( isset( $_COOKIE[session_name()] ) ) {
-		setcookie( session_name(), “”, time()-3600, “/” );
-	}
-	$_SESSION = array();
-	session_destroy();
+    if (!isset($_SESSION['loggedin']))  {
+        session_start();
 
-        header("Location: ../login");
- 
-	echo "Killing:".$_SESSION['username'];
+        if ( isset( $_COOKIE[session_name()] ) ) {
+            setcookie( session_name(), “”, time()-3600, “/” );
+        }
+        $_SESSION = array();
+        session_destroy();
+    
+            header("Location: ../login");
+     
+        echo "Killing:".$_SESSION['username'];
     }
 ?>
