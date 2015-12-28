aa3e393541b66403f748f3638cfdf80f2c1bc46f
sanjoy saha <sanjoysaha85@gmail.com>
2015-12-28 23:18:39 +0530

    yearly report bar chart

--- php/report.php
+++ php/report.php
@@ -9,9 +9,13 @@
         $table = "_".$month."_".$selyear."_";
         $sql="SELECT SUM(amount) AS TOTAL FROM $table";
         $result=mysql_query($sql);
-       
-        while ($row = mysql_fetch_array($result)) {
-           echo "@$table=".$row{'TOTAL'};
+
+        if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '".$table."'")) != 1)   {
+               echo "@$table=0";
+        } else {
+            while ($row = mysql_fetch_array($result)) {
+               echo "@$table=".$row{'TOTAL'};
+            }
         }
     }
 
