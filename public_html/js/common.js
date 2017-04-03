e2caac94e7fa895810751b557daba4e992ca7a59
Sanjoy Saha <sanjoysaha85@gmail.com>
2017-04-02 21:00:58 -0400

    Code optimization.

---
+++ public_html/js/common.js
@@ -0,0 +1,25 @@
+var categoryName  = ["Category",
+                     "Uncategorized", "Home", "Conveyance", "Groceries", "Gifts",
+                     "Shopping", "Miscellaneous", "Cigarette", "Bills", "Food",
+                     "Medical", "Kids", "Automobile"];
+
+var categoryColor = ["",
+                     "#999999", "#27AE60","#D98880","#B3EF43","#FF751A",
+                     "#C70039","#EB984E","#FF5733","#3f0080","#5DADE2",
+                     "#E6B700","#9966FF","#3973AC"];
+
+var monthName     = [ "",
+                      "January", "February", "March", "April", "May", "June", 
+                      "July", "August", "September", "October", "November", "December"];
+
+var weekdays      = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+                     "Thursday", "Friday", "Saturday"];
+
+function _(el){
+    return document.getElementById(el);
+}
+
+function __(el){
+    return parent.document.getElementById(el);
+}
+
